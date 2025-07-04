8f770edcae062d9f2563a1a7906ef549b4a37fab
venstnguyen@gmail.com <139813710+venstnguyen@users.noreply.github.com>
2025-07-03 23:08:59 -0600

    Finished Half of Required Methods!

--- src/Burner.java
+++ src/Burner.java
@@ -1,6 +1,6 @@
 public class Burner {
 	
-	enum temperature {
+	enum Temperature {
 		ICECOLD,
 		WARMINGUP,
 		GETTINGHOT,
@@ -8,15 +8,74 @@ public class Burner {
 		
 	}
 	
-	private temperature myTemp;
+	private Temperature myTemp;
 	private Setting mySetting;
 	private int clock;
 	boolean fireCheck = false;
 	private final static int TIME_LENGTH = 2;
 	
 	public Burner() {
-		myTemp = temperature.ICECOLD;
-		mySetting = 
+		myTemp = Temperature.ICECOLD;
+		mySetting = Setting.OFF;
 	}
+	
+	public Temperature getMyTemp() {
+		return myTemp;
+	}
+	
+	public void addTemp() {
+		clock = TIME_LENGTH;
+		
+		switch(mySetting) {
+		
+		case OFF:
+			mySetting = Setting.LOWHEAT;
+			break;
+		
+		case LOWHEAT:
+			mySetting = Setting.MEDIUMHEAT;
+			break;
+			
+		case MEDIUMHEAT:
+			mySetting = Setting.HIGHHEAT;
+			break;
+			
+		case HIGHHEAT:
+			return;
+		}
+		return;
+	}
+	
+	public void lowerTemp() {
+		clock = TIME_LENGTH;
+		
+		switch(mySetting) {
+		
+		case OFF:
+			return;
+		
+		case LOWHEAT:
+			mySetting = Setting.OFF;
+			break;
+			
+		case MEDIUMHEAT:
+			mySetting = Setting.LOWHEAT;
+			break;
+			
+		case HIGHHEAT:
+			mySetting = Setting.MEDIUMHEAT;
+			break;
+		}
+		return;
+	}
+	
+	public void updateTemperature() {
+		
+	}
+	
+	public void displayTemperature() {
+		
+	}
+	
 	
 }
